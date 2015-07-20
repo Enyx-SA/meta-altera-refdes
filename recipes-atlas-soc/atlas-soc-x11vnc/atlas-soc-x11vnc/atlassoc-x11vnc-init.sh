b0eca95850048bbb384897f2c064269ce1bda23f
Dalon Westergreen <dwesterg@gmail.com>
2015-07-19 17:06:56 -0700

    update x11init script per Rod

--- recipes-atlas-soc/atlas-soc-x11vnc/atlas-soc-x11vnc/atlassoc-x11vnc-init.sh
+++ recipes-atlas-soc/atlas-soc-x11vnc/atlas-soc-x11vnc/atlassoc-x11vnc-init.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
 
-# create this environment setup file for the shell in the x11vnc session
+X11VNC__SH_ENV="/home/root/.x11vnc_env"
+X11VNC_HOME="/home/root"
 
-cat <<EOF > /home/root/.x11vnc_env
+# create this environment setup file for the shell in the x11vnc session
+[ -f "${X11VNC__SH_ENV}" ] || {
+cat <<EOF > "${X11VNC__SH_ENV}"
+
+# this file is created by the script ‘/usr/bin/atlassoc-x11vnc-init.sh’.
+#
+
+# this file is assigned to the ENV environment variable which is evaluated
+# when /bin/sh is executed.  We use this to setup environment variables for
+# that shell.
 
 export PS1='\u@\h:\w$ '
-
 EOF
+}
 
 # then execute this command to start the x11vnc session
-FD_PROG=/usr/bin/xfce4-session /usr/bin/x11vnc -loop -repeat -env ENV=/home/root/.x11vnc_env -env HOME=/home/root -display WAIT:cmd=FINDCREATEDISPLAY-Xvfb
+FD_PROG=/usr/bin/xfce4-session \
+/usr/bin/x11vnc \
+-loop \
+-repeat \
+-env ENV="${X11VNC__SH_ENV}" \
+-env HOME="${X11VNC_HOME}" \
+-display WAIT:cmd=FINDCREATEDISPLAY-Xvfb
+
